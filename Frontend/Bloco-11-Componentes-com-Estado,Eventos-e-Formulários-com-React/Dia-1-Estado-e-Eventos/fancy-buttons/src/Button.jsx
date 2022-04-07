d73898ed5784a779199bcf625ae7813fcac75d5d
Thalys RD <tgbr66@gmail.com>
2022-04-07 15:48:23 -0300

    Exercício de state

--- Frontend/Bloco-11-Componentes-com-Estado,Eventos-e-Formulários-com-React/Dia-1-Estado-e-Eventos/fancy-buttons/src/Button.jsx
+++ Frontend/Bloco-11-Componentes-com-Estado,Eventos-e-Formulários-com-React/Dia-1-Estado-e-Eventos/fancy-buttons/src/Button.jsx
@@ -6,24 +6,34 @@ class Button extends Component {
         this.click1 = this.click1.bind(this);
         this.click2 = this.click2.bind(this);
         this.click3 = this.click3.bind(this);
+        this.state = {
+            button1: 0,
+            button2: 0,
+            button3:0
+        }
     }
 
     click1 (){
-        console.log("1");
-        console.log(this);
+        this.setState((estadoAnterior, _props ) => ({
+            button1: estadoAnterior.button1 + 1
+        }))
     }
     
      click2 (){
-        console.log("2");
-        console.log(this);
+        this.setState((estadoAnterior, _props ) => ({
+            button2: estadoAnterior.button2 + 1
+        }))
     }
     
     click3 () {
-        console.log("3");
-        console.log(this);}
+        this.setState((estadoAnterior, _props ) => ({
+            button3: estadoAnterior.button3 + 1
+        }))
+    }
+        
 
     render() {
-        return (<div><button onClick={this.click1}>1</button> <button onClick={this.click2}>2</button>  <button onClick={this.click3}>3</button> </div>)
+        return (<div><button onClick={this.click1}>{this.state.button1}</button> <button onClick={this.click2}>{this.state.button2}</button>  <button onClick={this.click3}>{this.state.button3}</button> </div>)   
     }
 }
 
